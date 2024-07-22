ca3df6d6d0ed4e5ae5d92018857fb9521a661690
ndl <ndl@gmail.com>
2024-07-22 22:17:08 +0700

    config router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
-function App() {
-  return (
-    <>
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
       <h1 className="text-3xl font-bold underline text-cyan-600">
         Hello world!
       </h1>
-    </>
-  );
+    ),
+  },
+]);
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
