f00356d9dc9bc3c4820873d5dc360dbeb918c09e
ndl <ndl@gmail.com>
2024-07-25 07:53:15 +0700

    jjj

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,11 @@
+const bcrypt = require('bcryptjs');
+
+
 const handleSignUp = (req, res) => {
-  console.log("data", req.body);
   res.send("run in handle sign in");
+  const {userName, email, password} = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10)
+  console.log("data", req.body);
 };
 
 module.exports = { handleSignUp };
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,7 +1,23 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     userName: {
-        type: string,
-    }
-})
+      type: string,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: string,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: string,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("user", userSchema);
